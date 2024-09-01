f5fb9d138dba9552e8b897676e5d2e93acac9c7f
six-two <git@six-two.dev>
2024-09-01 21:40:20 +0200

    Implemented toggle button for readthedocs theme

--- src/mkdocs_toggle_sidebar_plugin/readthedocs.js
+++ src/mkdocs_toggle_sidebar_plugin/readthedocs.js
@@ -1,27 +1,55 @@
 const setCombinedVisibility = (showNavigation, showTOC) => {
     // Navigation and TOC are merged so we just pick one of the values and trust them
     // @TODO: add support for combined bars in toggle-sidebar.js?
-    if (showNavigation) {
-        return "";
-    } else {
-        return `
-section.wy-nav-content-wrap {
-    margin-left: 0px;
+    let style = `
+.mkdocs-toggle-sidebar-button {
+    cursor: pointer;
+    width: 1em;
+    fill: darkgray;
+    display: flex;
+    margin-right: 5px;
 }
 
-nav.wy-nav-side, div.rst-versions {
-    display: none;
+/* Hide toggle button in mobile view */
+@media screen and (max-width: 768px) {
+    .wy-breadcrumbs > li.mkdocs-toggle-sidebar-container {
+        display: none;
+    }
 }
+`;
+    if (!showNavigation) {
+        // We can not hide the contents in mobile view, since then the hamburger menu would be empty
+        style += `
+@media screen and (min-width: 768px) {
+    section.wy-nav-content-wrap {
+        margin-left: 0px;
+    }
 
-div.wy-nav-content {
-    max-width: 1100px;
+    nav.wy-nav-side, div.rst-versions {
+        display: none;
+    }
+
+    div.wy-nav-content {
+        max-width: 1100px;
+    }
 }
 `;
     }
+
+    return style;
     
 }
 
-const addToggleButton = (toggleNavigation, toggleTOC) => {}
+const addToggleButton = (toggleNavigation, toggleTOC) => {
+    const toggleBtn = createDefaultToggleButton(toggleNavigation, toggleTOC);
+    toggleBtn.title = "Toggle Navigation"; // There is only one real sidebar, so we use this name for consistency
+    const li = document.createElement("li");
+    li.className = "mkdocs-toggle-sidebar-container";
+    li.append(toggleBtn);
+
+    const titleElement = document.querySelector("ul.wy-breadcrumbs");
+    titleElement.insertBefore(li, titleElement.firstChild);  
+}
 
 const registerKeyboardEventHandler = () => {
     // Native HTML key event handling
