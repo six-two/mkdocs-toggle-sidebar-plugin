6437bcfad9a32db5153fdb22fc0586beb8b29980
six-two <git@six-two.dev>
2024-09-10 17:07:02 +0200

    Fixed Material mode in mobile view (small screen)

--- src/mkdocs_toggle_sidebar_plugin/material.js
+++ src/mkdocs_toggle_sidebar_plugin/material.js
@@ -1,4 +1,7 @@
 const setCombinedVisibility = (showNavigation, showTOC) => {
+    // Hide the button when on mobile (and menu us shown as hamburger menu anyways).
+    // The exact max-width is taken from the styling of the 'body > header > nav > a' element
+    
     let style = `
 .mkdocs-toggle-sidebar-button {
     cursor: pointer;
@@ -6,31 +9,34 @@ const setCombinedVisibility = (showNavigation, showTOC) => {
     margin-left: 1rem;
 }
 
-/*
-Hide the button when on mobile (and menu us shown as hamburger menu anyways).
-The exact max-width is taken from the styling of the 'body > header > nav > a' element
-*/
-
 @media screen and (max-width: 76.1875em) {
     .mkdocs-toggle-sidebar-button {
         display: none;
     }
 }
 `;
-    if (!showTOC) {
-        style += `
-div.md-sidebar.md-sidebar--secondary {
-    display: none;
+// The TOC has a different break point than the navigation.
+// It can be seen on the nav.md-nav--secondary:nth-child(1) element (60em)
+// If the screen is smaller, it is shown in the navigation section if you click the nested hamburger menu
+if (!showTOC) {
+    style += `
+@media screen and (min-width: 60em) {
+    div.md-sidebar.md-sidebar--secondary {
+        display: none;
+    }
 }
 `;
-    }
-
+        }
+        
+    // We always have to show the navigation in mobile view, otherwise the hamburger menu is broken
     if (!showNavigation) {
         style += `
-div.md-sidebar.md-sidebar--primary {
-    display: none;
+@media screen and (min-width: 76.1875em) {
+    div.md-sidebar.md-sidebar--primary {
+        display: none;
+    }
 }
-`
+`;
     }
 
     return style;
